13df8716673c842461df058aba2186824582ad91
e <jinlugzyx@163.com>
2020-09-12 17:58:54 +0800

    test hello world

---
+++ Test.java
@@ -0,0 +1,14 @@
+/**
+* 这是一个打印hello world的类
+*@author by lujiaobei
+*/
+public class Test{
+        // 单行注释
+        /*
+          多行注释
+          多行注释
+        */
+        public static void main(String[] args){
+                System.out.print("hello world");
+        }
+}
